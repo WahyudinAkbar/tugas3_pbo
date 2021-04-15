1294139b98a713482262fc9bbc13eb8d83016e6d
WahyudinAkbar <wahyu.id.dev@gmail.com>
2021-04-15 12:19:31 +0800

    toUpperCase Berfungsi Untuk Membuat String yang dipilih menjadi huruf besar/kapital semua toLowerCase Berfungsi Untuk Membuat String yang dipilih menjadi huruf kecil semua substring(5) Berfungsi Untuk Mengambil potongan String yang dimulai dari index 5 sampai index terakhir dari String Kandangan yaitu ngan, K=0, A=1, N=2, D=3, A=4, N=5, dst. Substring(0, 4) Berfungsi Untuk Mengambil potongan String yang dimulai dari index 0 sampai index 4 dari String Kandangan yaitu Kand, K=0, A=1, N=2, D=3, A=4, N=5, dst.

--- src/string/OperasiString.java
+++ src/string/OperasiString.java
@@ -1,5 +1,7 @@
 package string;
 
+import java.util.Locale;
+
 public class OperasiString {
     public static void main(String[] args) {
         String kota = "Kandangan";
@@ -9,5 +11,10 @@ public class OperasiString {
         char[] wahyuChar = { 'W', 'A', 'H', 'Y', 'U'};
         String wahyuString = new String(wahyuChar);
         System.out.println(wahyuString);
+
+        System.out.println(kota.toUpperCase());
+        System.out.println(kota.toLowerCase());
+        System.out.println(kota.substring(5));
+        System.out.println(kota.substring(0,4));
     }
 }
